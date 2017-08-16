c461f6c647c229a1918267e4a8f00969d385a278
Lislie <khalilfong@Live.cn>
2017-08-16 15:09:08 +0800

    1
    1

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,28 +5,62 @@
 </template>
 
  <script type="text/ecmascript-6">
-  import { getSingerList } from 'api/singer'
-  import { ERR_OK } from 'api/config'
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
 
-  export default {
-    data() {
-      return {
-        singers: []
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
+export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(this._normalizeSinger(this.singers))
+        }
+      })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
       }
-    },
-    created() {
-      this._getSingerList()
-    },
-    methods: {
-      _getSingerList() {
-        getSingerList().then((res) => {
-          if (res.code === ERR_OK) {
-            this.singers = res.data.list
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) { // 如果有 没有 key 给他添加类
+          map[key] = {
+            title: key,
+            items: []
           }
-        })
-      }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 为了得到有序列表，我们需要处理 map
+      console.log(map)
     }
   }
+}
 </script>
 
 <style lang="stylus" scoped rel="stylesheet/stylus">
